ea9fbe04736af85e4eb9f3bbb2e981249ffc7d48
Ignacio Albarrán <ignacioalbarran851@gmail.com>
2020-11-23 13:32:30 +0100

    First commit with basic files and routes

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const PORT = 3000
+
+
+app.listen(PORT, () => {
+  console.log('SERVER EN MARCHA, ADELANTE!!!');
+});
+
+app.get('/', (req, res) => {
+  res.send('YOU ARE IN THE MAIN PAGE')
+});
+
+app.get('/contact-us', (req, res) => {
+  res.send('This is the contact us page')
+});
+
+app.get('/request/country', function (req, res) {
+  res.json(films)
+});
+
+app.post('/request/food', (req, res) => {
+  console.log(`*********************`)
+  console.log(`*********************`)
+  console.log(`*********************`)
+  console.log(`*********************`)
+  console.log(`*********************`)
+  console.log(`*********************`)
+  console.log(`*********************`)
+  console.log(`*********************`)
+  console.log(`*********************`)
+  console.log(`*********************`)
+  console.log(`*********************`)
+  console.log(`*********************`)
+  console.log(`*********************`)
+  console.log(`*********************`)
+  console.log(`*********************`)
+  console.log(`*********************`)
+
+  res.json('My Favourite food is ' + res.body.food)
+});
+
+
+
